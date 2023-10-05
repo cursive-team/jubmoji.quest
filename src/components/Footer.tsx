00f6a8d5b97e38faf5778ecc1b5a6e70433ddb83
Vivek Bhupatiraju <vivekab@mit.edu>
2023-10-05 23:27:21 +0200

    added page routes to test speed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div>footer</div>
-  )
+    <>
+      <Link href="/" style={{ color: "black" }}>
+        Home
+      </Link>
+      <Link href="/card-profile" style={{ color: "black" }}>
+        Card Profiles
+      </Link>
+      <Link href="/collection" style={{ color: "black" }}>
+        Collection
+      </Link>
+      <Link href="/quests" style={{ color: "black" }}>
+        Quests
+      </Link>
+    </>
+  );
 }
