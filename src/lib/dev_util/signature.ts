d4a3b42d4e976d9a14b9f6967a5aff4a79e61844
AndrewCLu <andrewclu98@gmail.com>
2023-10-13 14:06:20 -0400

    add signature utils for arx cards

--- src/lib/dev_util/signature.ts
+++ src/lib/dev_util/signature.ts
@@ -1,10 +1,38 @@
-export const hashMessage = (msg: string): bigint => {
-  throw new Error("Not implemented");
+const keccak256 = require("keccak256");
+import { sha256 } from "js-sha256";
+import { extendHexString } from "./utils";
+
+// Recovers the message hash of an arbitrary message signature
+// https://github.com/arx-research/libhalo/blob/master/docs/halo-command-set.md#command-sign
+export const recoverArbitraryMessageHash = (msg: string): bigint => {
+  const msgString =
+    "\x19Ethereum Signed Message:\n" + msg.length.toString() + msg;
+
+  return keccak256(Buffer.from(msgString)).toString("hex");
 };
 
-export const recoverMessageFromNonceAndRand = (
+// Recovers the message hash of a counter signature
+// https://github.com/arx-research/libhalo/blob/master/docs/halo-command-set.md#command-sign_random
+export const recoverCounterMessageHash = (
   msgNonce: number,
   msgRand: string
 ): string => {
-  throw new Error("Not implemented");
+  // Nonce occupies the first 4 bytes of the message
+  const nonceString = extendHexString(msgNonce.toString(16), 8);
+  // Randomness occupies the next 28 bytes of the message
+  const randString = extendHexString(msgRand, 56);
+  const msgString = nonceString + randString;
+
+  const msgBuffer = Buffer.from(msgString, "hex");
+  const hash = sha256.create();
+
+  return hash
+    .update(
+      Buffer.concat([
+        Buffer.from([0x19]),
+        Buffer.from("Attest counter pk2:\n", "utf8"),
+      ])
+    )
+    .update(msgBuffer)
+    .hex();
 };
--- src/lib/dev_util/utils.ts
+++ src/lib/dev_util/utils.ts
@@ -11,3 +11,10 @@ export const getRandomNullifierRandomness = (): bigint => {
 
   return bytesToBigInt(uuidParse(uuid));
 };
+
+// Adjusts a hex string to be a certain length by adding a leading 0 if necessary
+export const extendHexString = (hex: string, desiredLength: number): string => {
+  const zeros = "0".repeat(length - hex.length);
+
+  return zeros + hex;
+};
