3a3694a51cb696a11cd3a211d8cc893d01be203b
Kalidou Diagne <kld.diagne@gmail.com>
2023-10-16 18:26:28 +0200

    add manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+
         <meta name="application-name" content="Jumboji" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
