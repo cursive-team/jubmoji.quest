605fb74a02af3d4e4a60e79ef941191142fd7e8f
Andrew Lu <andrewclu98@gmail.com>
2023-10-17 02:12:30 -0400

    Create dev harness for jubmoji.quest (#20)

---
+++ src/lib/dev_util/proving.ts
@@ -0,0 +1,30 @@
+import {
+  derDecodeSignature,
+  getPublicInputsFromSignature,
+} from "babyjubjub-ecdsa";
+import { NonceSignature } from "../dev_mockData/nonceSigs";
+import { Jubmoji } from "../dev_types";
+import { recoverCounterMessageHash } from "./signature";
+import { getCardPubKeyFromIndex } from "./utils";
+
+export const getJubmojiFromNonceSignature = ({
+  nonce,
+  rand,
+  sig,
+  pubKeyIndex,
+}: NonceSignature): Jubmoji => {
+  const decodedSig = derDecodeSignature(sig);
+  const msgHash = recoverCounterMessageHash(nonce, rand);
+  const pubKey = getCardPubKeyFromIndex(pubKeyIndex);
+  const { R, T, U } = getPublicInputsFromSignature(decodedSig, msgHash, pubKey);
+
+  return {
+    pubKeyIndex,
+    sig,
+    msgNonce: nonce,
+    msgRand: rand,
+    R: R.serialize(),
+    T: T.serialize(),
+    U: U.serialize(),
+  };
+};
