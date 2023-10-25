b47d79b2e0144ce0a02ba164f50bab6fa74afda4
Vivek Bhupatiraju <vivekab@mit.edu>
2023-10-25 04:09:33 -0400

    add redirect links

--- apps/jubmoji-quest/next.config.js
+++ apps/jubmoji-quest/next.config.js
@@ -6,6 +6,22 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, readline: false };
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/airtable",
+        destination:
+          "https://airtable.com/apprikYUs5WuXJRbN/tblEFhdjC2nEWP4cQ/viwFeEgb7Kfy08Jaa?blocks=hide",
+        permanent: true,
+      },
+      {
+        source: "/cardforyou",
+        destination:
+          "https://airtable.com/apprikYUs5WuXJRbN/shrep6Pp59ukNJp34/tblEFhdjC2nEWP4cQ",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
