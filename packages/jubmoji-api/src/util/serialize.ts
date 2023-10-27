967db58ab10b3e6239f1fcf3462c78fa77b715c4
Vivek Bhupatiraju <vivekab@mit.edu>
2023-10-27 06:40:18 -0400

    adding async frontend collection code

--- packages/jubmoji-api/src/util/serialize.ts
+++ packages/jubmoji-api/src/util/serialize.ts
@@ -1,4 +1,8 @@
+import { serialize } from "v8";
+import { JubmojiInCollection } from "../proofs";
 import { Jubmoji } from "../types";
+import { NonceSignature } from "../data";
+import { getJubmojiFromNonceSignature } from "./proving";
 
 export const serializeJumboji = (jubmoji: Jubmoji): string => {
   return JSON.stringify(jubmoji);
@@ -14,4 +18,36 @@ export const serializeJubmojiList = (jubmojis: Jubmoji[]): string => {
 
 export const deserializeJubmojiList = (serialized: string): Jubmoji[] => {
   return JSON.parse(serialized);
+};
+
+export const succinctSerializeJubmojiList = (jubmojis: Jubmoji[]): string => {
+  let serialization = "";
+  for (const jubmoji of jubmojis) {
+    // use base64 encoding to reduce length by 33%
+    serialization += [
+      Buffer.from(jubmoji.sig, "hex").toString("base64"),
+      jubmoji.msgNonce,
+      Buffer.from(jubmoji.msgRand, "hex").toString("base64"),
+      jubmoji.pubKeyIndex,
+    ].join(";");
+    serialization += ",";
+  }
+  return serialization.substring(0, serialization.length - 1);
+};
+
+export const succinctDeserializeJubmojiList = (
+  serialized: string
+): Jubmoji[] => {
+  const jubmojis: Jubmoji[] = [];
+  for (const serializedJubmoji of serialized.split(",")) {
+    const [sig, msgNonce, msgRand, pubKeyIndex] = serializedJubmoji.split(";");
+    const nonceSig: NonceSignature = {
+      sig: Buffer.from(sig, "base64").toString("hex"),
+      nonce: parseInt(msgNonce),
+      rand: Buffer.from(msgRand, "base64").toString("hex"),
+      pubKeyIndex: parseInt(pubKeyIndex),
+    };
+    jubmojis.push(getJubmojiFromNonceSignature(nonceSig));
+  }
+  return jubmojis;
 };
