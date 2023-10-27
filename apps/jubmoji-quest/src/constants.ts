092ec12514239b8609810d24d231aa2ce9158e6c
Vivek Bhupatiraju <vivekab@mit.edu>
2023-10-27 10:49:04 -0400

    added e2e copy paste and google wallet buttons

--- apps/jubmoji-quest/src/constants.ts
+++ apps/jubmoji-quest/src/constants.ts
@@ -1,4 +1,3 @@
-const GOOGLE_OBJECT_SUFFIX = crypto.randomUUID();
 const GOOGLE_WALLET_ISSUER_ID = "3388000000022248072";
 
 export const GOOGLE_PASS_CREDENTIALS = {
@@ -19,7 +18,7 @@ export const GOOGLE_PASS_CREDENTIALS = {
 
 export const APP_CONFIG = {
   APP_NAME: "Jubmojis",
-  APP_SLUG: "jubmoji.quest",
+  APP_SLUG: "Jubmojis Backup",
   WEBSITE_URI: "http://jubmoji.quest",
   EVENT_NAME: "Devconnect",
   COLLECTION_SIZE: 201,
@@ -33,13 +32,18 @@ export const APP_CONFIG = {
 // Google Wallet Object Constants
 export const GOOGLE_WALLET = {
   ISSUER_ID: GOOGLE_WALLET_ISSUER_ID,
-  CLASS_ID: `${GOOGLE_WALLET_ISSUER_ID}.sigmoji_sbc_final_1`,
-  OBJECT_ID: `${GOOGLE_WALLET_ISSUER_ID}.${GOOGLE_OBJECT_SUFFIX}`,
-  HEX_BACKGROUND_COLOR: "#1A1A1A", // Hex color code
-  LOGO_URI: "https://i.ibb.co/bbvN62L/logo.png",
-  BARCODE_URI: "http://nfctap.xyz",
-  HERO_IMAGE_URI:
-    "https://raw.githubusercontent.com/project-vinyl/nfctap.xyz/579e0cdbda8fab976230deb0594b810f3db747ff/models/sigmoji.pass/strip%403x.png",
+  CLASS_ID: `${GOOGLE_WALLET_ISSUER_ID}.jubmoji_pass_3`,
+  OBJECT_ID: (serial: string) => `${GOOGLE_WALLET_ISSUER_ID}.${serial}`,
+  HEX_BACKGROUND_COLOR: "#1E1E1F", // Hex color code
+  LOGO_URI: "https://i.ibb.co/370zVNG/logo-2x.png",
+  HERO_IMAGE_URI: "https://i.ibb.co/Ht2S11B/strip.png",
+  /**
+   * It's okay to expose our Google Wallet API private key here because the
+   * "useful" information in the passes are the signatures from the cards,
+   * which cannot be forged. Without those signatures, no valid ZKPs can be made.
+   * This is a necessary hack to avoid sending the user's private signatures
+   * to the server to get signed, which is usually how passes are handled.
+   */
   PRIVATE_KEY:
     "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCiaTp66TwI472V\nhZpX+RQFw+OmzuqU3t/Vt5Gk9FQ/3tpwFr1HKB6iCYAxCv/CyGRueuEYiGF/6cPY\ndgVE4OrdRgG8A8kaL2SOSGqmDL2WkKZoVpxxNomutWFwGNkCFys6ZFAac01znSKC\nj0qq7rReyHKva9EGXK9Zmx0vNDytpnVVjNZgUHbEHyD9noQM+OuZW6SoLxb2Rii/\nHczjsud8q9524OGC0eKipaHis9temZdcQEY+15d4h8Ct6S/tEwdUipNPBIobNX00\nLXAoLGabg5k5hShs2bXA1taPYQR5Wxc6vmn/W+KjzHpJhAmfk0il186ZCpg1//YP\n4CyHendVAgMBAAECggEADaDt7t1+k7JkuNTeyE4W/4raIifP6JExUFJYrvJv1XJC\ne1B1LrDvrSJF9xeYscQ2JU07cV8Ua3L248W3mHmVS98xixD4QbHDauspw+hiym02\nAaBu/VMZx6bh19HF+BZworRDQoz1bWO8vx7+IYzpZuDEOvM9tZ52enr5PnpQMzQY\nAKLygZFbLFYPS9+pU7sEPpy8+QMT+Djwsf46Ufd4mQket+wqMS2jN+zTA8pCOm3Y\nY0e7oRJZeR7XSV4NLNGAaoADUPxafuDpD9wAHs/+SMEsrBPX2OzA7a6gpm2R1qVC\n769h5UDfeXZ4Ox4eAmSybRX0bQJP75e3K9QRBjFJ8QKBgQDNF8FOGJ6qaHZpGVR4\nWLFwa9cEgfbQusau82s23Fb/GQ+IQFroh5hFTyNz6qLX7n1UN4z89aBI8HupPsW5\nITNrc9dVcc+DvO5nabExCTg/119EVmb65GAnXC8Abwx0BMpVUyIsp2h/62Lgz1xX\nt3iJXHM0hW98Be8vuWrrcEZe/QKBgQDKuVca3UPcJxf3RbJIzkLfuBjn2PHqVFAp\nbkalYY8KIycLAo5Yn/7fqBZZmqDlzOFooh9y3SzasaXlYi6WYutCORP6/trKB3GQ\ngbO2IkbXqq5gysuCVYt2HtoMTrrqJYT5/+T4QeuT9E0gGESkl8vybn+ljYIpe2wj\ntt/oDWPlOQKBgCezJ3oEjgISgolJm+5HzOEkHtUCi1JQNVF8UQ6njDeJFFadjntg\nc/tGnGg9zRPbWGtK7YGx0jsMQgO64O87HAb6v34Mr8yib8uYy/HgBQFmoUxLJVf/\niRVVZMvCszhqDPiDRSQ7q6DHYbZ+rTNVkoGUYcCS68eoJmqoi6fTbevJAoGACL9u\nrs4Ve46ETjtHsjSUVisPiBGofsAcpW0Ix2IqVfmmWadGlTRyPxx1unAduUzZ/TXh\nvYclxFoeGoPeFsc196mW3yHYNxeTIvU12AoqnGvVEoAtv/YJCpPSPO24yxnLXy5X\npw7RJ5VHLrddXuzd9BwojfBJSZj11aSyjMTnJqECgYB06E9uHgvFefq4P50E5xxl\nMQELrsfDGP4L4UkUQvQ8UAsBXNnEFaiuFarQR+wbYv/JoU3/6S/n/C5RtFdZb6b/\nGmwUtIa/vZ5ojrdRsup2enF8Z91Q0b8d9PobeIRYn9pbBOC7NJ5JNlQRVBtEWQwR\nOGUNQQVf5JXVDQuti4DAuA==\n-----END PRIVATE KEY-----\n",
 };
