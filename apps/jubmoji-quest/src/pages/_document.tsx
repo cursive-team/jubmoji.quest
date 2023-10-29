58ae59b13871e5450b7bcc7ec00f99993ecab7a7
Andrew Lu <andrewclu98@gmail.com>
2023-10-29 15:41:01 +0000

    favicon (#96)

--- apps/jubmoji-quest/src/pages/_document.tsx
+++ apps/jubmoji-quest/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="jubmoji.quest" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
