6a5ac60ae6ceec38f390667112bf43ff41c916d7
Vivek Bhupatiraju <vb7401@users.noreply.github.com>
2023-10-30 00:23:27 +0100

    Update infoFaqs.ts (#105)

--- apps/jubmoji-quest/src/common/infoFaqs.ts
+++ apps/jubmoji-quest/src/common/infoFaqs.ts
@@ -8,12 +8,12 @@ type InfoProps = {
 
 export const FAQS: InfoProps[] = [
   {
-    title: "What is Jubmoji Quest?",
-    description: `Jubmoji.quest is place to keep personal, provable digital mementos from people that you meet IRL. Each time you tap a card, you collect a Jubmoji, a unique signature that you can store privately and share as you wish!`,
+    title: "What is jubmoji.quest?",
+    description: `Jubmoji.quest is place to keep personal, provable digital mementos from people you meet and places you visit IRL. Each time you tap a card, you collect a Jubmoji, a unique cryptographic signature that you can store privately and share as you wish!`,
   },
   {
     title: "What are quests?",
-    description: `Quests are challenges to collect special sets of jubmojis. Completing quests gives you the opportunity to wield cool powers (see below) . You can choose to publicly prove your quest scores and compete in leaderboards. Keep completing quests to become a legendary collector in the community!`,
+    description: `Quests are challenges to collect special sets of jubmojis. Completing quests gives you the opportunity to wield cool powers (see next) . You can choose to publicly prove your quest scores and compete in leaderboards. Keep completing quests to become a legendary collector in the community!`,
   },
   {
     title: "What are powers?",
@@ -26,12 +26,16 @@ export const FAQS: InfoProps[] = [
   {
     title: "Back up your data",
     description:
-      "All collected jubmojis are stored in your browser's temporary storage, so it's important to back up your data to your mobile wallet or a password manager upon collection of each jubmoji. Then you can  restore your collection if you accidentally close your browser tab.",
+      "All collected jubmojis are stored in your browser's temporary storage, so it's important to back up your data to your mobile wallet or a password manager upon collection of each jubmoji. Then you can restore your collection if you accidentally close your browser tab.",
+  },
+  {
+    title: "Need help tapping?",
+    description: `Click the “Assisted tap” button on the main page, which will make it easier to collect Jubmojis from the cards. Hold it for 1-2s against the top of your phone if it's an iPhone, and the center of your phone if it's an Android.` 
   },
   {
     title: `What is the “Jub” in Jubmoji?`,
-    description: `"Jub" comes from the term "Baby Jubjub Curve," which is a mathematical equation used to verify data securely.
-
-    We use the Baby Jubjub Curve alongside zero-knowledge proofs (ZKPs) to provide a secure and private way to manage your Jubmojis. ZKPs let you prove something without revealing all information. Simply put, the Baby Jubjub Curve is a special lock for your memories, letting you safely share and verify your jubmojis without giving up your full data and privacy 🗝️🔒!`,
+    description: `"Jub" comes from the term "Baby Jubjub Curve," the elliptic curve used for the card's signatures. It was found in 2019 by Barry WhiteHat, Marta Bellés, and Jordi Baylina.
+    
+    We use the Baby Jubjub Curve alongside zero-knowledge proofs (ZKPs) to provide a secure and private way to manage your Jubmojis. ZKPs let you prove something without revealing all information. Simply put, the Baby Jubjub Curve provides a special lock for your memories, letting you safely share and verify your jubmojis without giving up your full data and privacy 🗝️🔒!`,
   },
 ];
