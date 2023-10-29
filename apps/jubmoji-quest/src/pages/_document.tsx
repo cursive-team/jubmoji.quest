50120d475b00e1e6dd6edac8d1b4a811e42e20b9
AndrewCLu <andrewclu98@gmail.com>
2023-10-29 19:05:33 +0300

    update page title

--- apps/jubmoji-quest/src/pages/_document.tsx
+++ apps/jubmoji-quest/src/pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head title="jubmoji.quest" />
+    <Html lang="en" title="jubmoji.quest">
+      <Head />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
