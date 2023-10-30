bb416c1a8ebcf9f1339aa05bd0b2e5424bab345e
Kalidou Diagne <kld.diagne@gmail.com>
2023-10-30 12:53:55 +0100

    onboarding add swiping

--- apps/jubmoji-quest/src/components/Onboarding.tsx
+++ apps/jubmoji-quest/src/components/Onboarding.tsx
@@ -1,5 +1,5 @@
 import { classed } from "@tw-classed/react";
-import React, { Children } from "react";
+import React, { Children, useRef } from "react";
 import { Button } from "./ui/Button";
 import { Icons } from "./Icons";
 import { useRouter } from "next/router";
@@ -22,17 +22,39 @@ const OnboardingIndicator = classed.div("h-2 border border-shark-600 grow", {
 const Onboarding = ({ children }: OnboardingProps) => {
   const router = useRouter();
   const [activeIndex, setActiveIndex] = React.useState(0);
+  const startX = useRef(0);
+  const startY = useRef(0);
+  const endX = useRef(0);
+  const endY = useRef(0);
 
   const arrayChildren = Children.toArray(children);
 
-  const handleNext = () => {
-    if (activeIndex < arrayChildren.length - 1) {
-      setActiveIndex(activeIndex + 1);
-    }
+  const handleSwipe = () => {
+    const direction = endX.current - startX.current < 0 ? "right" : "left";
+
+    const swipeIndex =
+      direction === "right" ? activeIndex + 1 : activeIndex - 1;
+
+    if (swipeIndex < 0 || swipeIndex > arrayChildren.length - 1) return; // can't go past the first or last slide
+
+    setActiveIndex(swipeIndex);
   };
+
   return (
     <div className="flex flex-col gap-8 w-full">
-      <div onClick={handleNext}>{arrayChildren[activeIndex]}</div>
+      <div
+        onTouchStart={(event: React.TouchEvent<HTMLDivElement>) => {
+          startX.current = event.touches[0].clientX;
+          startY.current = event.touches[0].clientY;
+        }}
+        onTouchEnd={(event: React.TouchEvent<HTMLDivElement>) => {
+          endX.current = event.changedTouches[0].clientX;
+          endY.current = event.changedTouches[0].clientY;
+          handleSwipe();
+        }}
+      >
+        {arrayChildren[activeIndex]}
+      </div>
       <div className="flex gap-2">
         {arrayChildren.map((child, index) => (
           <OnboardingIndicator
